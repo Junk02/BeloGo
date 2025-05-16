58e0761c00fdb6f99421d57e5a42e23531c1642f
MaksSh <Makssheshko986@gmail.com>
2025-05-16 16:23:17 +0300

    Revert "профиль"
    This reverts commit 1ce6646710df0661a15e82b8b5621a640501165e.

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -27,29 +27,3 @@ document.getElementById('logoutBtn').addEventListener('click', async () => {
 });
 
 loadProfile();
-
-// врячется хедер
-document.addEventListener('DOMContentLoaded', function () {
-    const navbar = document.getElementById('mainNavbar');
-    let lastScroll = 0;
-
-    window.addEventListener('scroll', function () {
-        const currentScroll = window.pageYOffset;
-
-        if (currentScroll <= 0) {
-            // Вверху страницы - показываем хедер
-            navbar.classList.remove('navbar-hide');
-            return;
-        }
-
-        if (currentScroll > lastScroll && !navbar.classList.contains('navbar-hide')) {
-            // Прокрутка вниз - скрываем хедер
-            navbar.classList.add('navbar-hide');
-        } else if (currentScroll < lastScroll && navbar.classList.contains('navbar-hide')) {
-            // Прокрутка вверх - показываем хедер
-            navbar.classList.remove('navbar-hide');
-        }
-
-        lastScroll = currentScroll;
-    });
-});
