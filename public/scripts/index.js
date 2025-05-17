d0e242c173ca7e7ac33de02a0bb451791ce225ee
Jun_k01 <kleshchukevichart@gmail.com>
2025-05-17 10:25:28 +0300

    removed unnecessary code
    The old header animation which is now made in another file

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -11,24 +11,4 @@ document.addEventListener('DOMContentLoaded', function () {
                 authLink.href = '/pages/profile.html';
             }
         });
-    
-    window.addEventListener('scroll', function () {
-        const currentScroll = window.pageYOffset;
-
-        if (currentScroll <= 0) {
-            // Вверху страницы - показываем хедер
-            navbar.classList.remove('navbar-hide');
-            return;
-        }
-
-        if (currentScroll > lastScroll && !navbar.classList.contains('navbar-hide')) {
-            // Прокрутка вниз - скрываем хедер
-            navbar.classList.add('navbar-hide');
-        } else if (currentScroll < lastScroll && navbar.classList.contains('navbar-hide')) {
-            // Прокрутка вверх - показываем хедер
-            navbar.classList.remove('navbar-hide');
-        }
-
-        lastScroll = currentScroll;
-    });
 });
--- public/scripts/loadimg.js
+++ public/scripts/loadimg.js
@@ -22,29 +22,6 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
 
-    const navbar = document.getElementById('mainNavbar');
-    let lastScroll = 0;
-
-    window.addEventListener('scroll', function () {
-        const currentScroll = window.pageYOffset;
-
-        if (currentScroll <= 0) {
-            // Вверху страницы - показываем хедер
-            navbar.classList.remove('navbar-hide');
-            return;
-        }
-
-        if (currentScroll > lastScroll && !navbar.classList.contains('navbar-hide')) {
-            // Прокрутка вниз - скрываем хедер
-            navbar.classList.add('navbar-hide');
-        } else if (currentScroll < lastScroll && navbar.classList.contains('navbar-hide')) {
-            // Прокрутка вверх - показываем хедер
-            navbar.classList.remove('navbar-hide');
-        }
-
-        lastScroll = currentScroll;
-    });
-
     // Инициализация карты
     function initMap() {
         map = L.map('map').setView([53.9, 27.5667], 7); // Центр Беларуси
