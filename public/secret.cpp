c3797e940c1d2aa97e6bdd657e15d5065d98921d
MaksSh <Makssheshko986@gmail.com>
2025-05-19 12:06:29 +0300

    ....????

--- public/secret.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    setlocale(0, "");
-    for(int i;i;i)
-        cout << "Сыцько++" << endl << "   +" << endl << "  c++" << endl << "   =" << endl << " LOVE";
-}
