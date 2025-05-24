c78a5a28943bc51cf4b80b06513c5d3ebaf61331
Jun_k01 <kleshchukevichart@gmail.com>
2025-05-24 14:21:28 +0300

    Update loadHeader.js

--- public/partials/loadHeader.js
+++ public/partials/loadHeader.js
@@ -1,4 +1,4 @@
-fetch('/public/partials/header.html')
+fetch('/partials/header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header-placeholder').innerHTML = data;
