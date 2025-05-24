68f43e5dd5ae5432af4a88087c4ea41a3d94ef28
Jun_k01 <kleshchukevichart@gmail.com>
2025-05-24 14:20:41 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -54,7 +54,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public/')));
 console.log(path.join(__dirname, '../public'));
 app.use(express.urlencoded({ extended: true }));
 // Настройка сессий
